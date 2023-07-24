8b9c1628886596bc0b7c7998c82fe4e97c6e7218
codarsssss <codarsssss@gmail.com>
2023-07-24 15:22:10 +0300

    get files from template

--- linteq/linteq_app/views.py
+++ linteq/linteq_app/views.py
@@ -7,4 +7,9 @@ def index(request):
     context = {
         'title': 'Linteq'
     }
+
+    if request.method == 'POST' and request.FILES:
+        file = request.FILES['file_input']
+        print(file)
+
     return render(request, 'linteq_app/index.html', context=context)
