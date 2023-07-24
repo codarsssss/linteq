08722354df0d587b76a352ca7911fb313efebb9b
Nikita <109899745+vvetta@users.noreply.github.com>
2023-07-24 16:12:10 +0300

    transcription_video

--- linteq/linteq_app/trascription_logic.py
+++ linteq/linteq_app/trascription_logic.py
@@ -3,25 +3,11 @@ import whisper
 
 def transcription_audio(file_input):
     file = file_input.read()
-    with open('audio.mp3', 'wb') as f:
+    with open('audio.mp4', 'wb') as f:
         f.write(file)
 
-    model = whisper.load_model("base")
+    model = whisper.load_model('base')
+    option = whisper.DecodingOptions(language='ru', fp16=False)
+    result = model.transcribe('audio.mp4')
 
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio('audio.mp3')
-    audio = whisper.pad_or_trim(audio)
-
-    # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    # detect the spoken language
-    _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
-
-    # decode the audio
-    options = whisper.DecodingOptions()
-    result = whisper.decode(model, mel, options)
-
-    # print the recognized text
-    print(result.text)
+    print(result)
