6bf9175bf79f5bd4ea55da74d0ab0c26388a4358
Nikita <109899745+vvetta@users.noreply.github.com>
2023-07-25 16:14:39 +0300

    save_subtitles

--- linteq/linteq_app/trascription_logic.py
+++ linteq/linteq_app/trascription_logic.py
@@ -1,6 +1,37 @@
 import whisper
 from whisper import DecodingOptions
-from whisper.utils import WriteSRT, SubtitlesWriter, ResultWriter
+from whisper.utils import WriteSRT, ResultWriter, WriteVTT, WriteTXT, WriteJSON
+
+
+
+def write_some_files(transcription_result: dict, options: dict, file, output_dir: str):
+    result_writer = ResultWriter(output_dir)
+
+    # Создание srt
+
+    srt_writer = WriteSRT(result_writer)
+    with open("media/subtitles.srt", "w") as file:
+        srt_writer.write_result(transcription_result, file, options)
+
+    # Создание vtt
+
+    vtt_writer = WriteVTT(result_writer)
+    with open("media/subtitles.vtt", "w") as file:
+        vtt_writer.write_result(transcription_result, file, options)
+
+    # Создание txt
+
+    txt_writer = WriteTXT(result_writer)
+    with open("media/subtitles.txt", "w") as file:
+        txt_writer.write_result(transcription_result, file, options)
+
+    # Создание json
+
+    json_writer = WriteJSON(result_writer)
+    with open("media/subtitles.json", "w") as file:
+        json_writer.write_result(transcription_result, file, options)
+
+    print('Done!')
 
 
 def transcription_video(file_input, file_extension):
@@ -15,59 +46,11 @@ def transcription_video(file_input, file_extension):
 
     result = model.transcribe(f"audio.{file_extension}")
 
-    output_dir = "/"  # Указать существующую директорию
-    result_writer = ResultWriter(output_dir)
+    output_dir = "/"
 
-    # Создание объекта WriteSRT
-    srt_writer = WriteSRT(result_writer)
-
-    # Открытие файла для записи субтитров
-    with open("subtitles.srt", "w") as file:
-    # Запись результатов в файл формата SRT
-        srt_writer.write_result(result, file, options={"max_line_width": 80,
-                                                       "max_line_count": 3,
-                                                       "highlight_words": False})
-
-    return result['text']
-
-
-#def transcription_audio(file_input, file_extension):
-    # Обработка аудио
-
-#     file = file_input.read()
-#     with open(f"audio.{file_extension}", 'wb') as f:
-#         f.write(file)
-
-#     model = whisper.load_model("base")
-
-#     audio = whisper.load_audio("audio.mp3")
-#     audio = whisper.pad_or_trim(audio)
-
-#     mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-#     _, probs = model.detect_language(mel)
-#     print(f"Detected language: {max(probs, key=probs.get)}")
-
-#     options = whisper.DecodingOptions(language='ru', fp16=False)
-#     result = whisper.decode(model, mel, options)
-
-#     return result.text
+    options = {"max_line_width": 80,
+                "max_line_count": 3,
+                "highlight_words": False}
     
+    write_some_files(result, options, file, output_dir)
 
-# def choice_audio_or_video_format(file):
-#     # Выбор функции
-#     file_extension = str(file).split('.')[-1]
-
-#     audio_format = ['m4a', 'wav', ]
-#     video_format = ['mp3', 'webm', 'mp4']
-
-#     if file_extension in audio_format:
-#         print('audio')
-#         return transcription_audio(file, file_extension)
-        
-#     elif file_extension in video_format:
-#         print('video')
-#         return transcription_video(file, file_extension)
-
-#     else:
-#         print('Формат не поддерживается.')
