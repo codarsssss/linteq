0251e7b64530688cbf9df4c75d1a59bd3a6f8997
codarsssss <codarsssss@gmail.com>
2023-07-26 20:14:07 +0300

    limit_file_size logic

--- linteq/linteq_app/static/linteq_app/js/limit_file_size.js
+++ linteq/linteq_app/static/linteq_app/js/limit_file_size.js
@@ -1,9 +1,15 @@
+const inputFile = document.getElementById('file');
+const button = document.getElementById('submit');
+const message= document.getElementById('er_message')
+inputFile.addEventListener('change', function() {
+  const selectedFile = inputFile.files[0];
+  if (selectedFile.size > 5000000) {
+    console.log('File size is bigger than 5Mb');
+    button.disabled=true;
+    message.classList.remove('d-none');
+  }
+  else {
+    message.classList.add('d-none')
+  }
 
-// let file=document.getElementById('file');
-//
-// file.addEventListener('on_change', function (e){
-//     e.stopPropagation();
-//     if (){
-//
-//     }
-// })
+});
