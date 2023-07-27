29990cd171183a73558c41ef11a8ee32ff763d1e
codarsssss <codarsssss@gmail.com>
2023-07-27 16:49:58 +0300

    preload gif add, disabled button logic

--- linteq/linteq_app/static/linteq_app/js/limit_file_size_button_logic.js
+++ linteq/linteq_app/static/linteq_app/js/limit_file_size_button_logic.js
@@ -1,14 +1,15 @@
 const inputFile = document.getElementById('file');
 const button = document.getElementById('submit');
 const message= document.getElementById('er_message')
+
+button.disabled=true;
 inputFile.addEventListener('change', function() {
   const selectedFile = inputFile.files[0];
   if (selectedFile.size > 1073741824) {
-    console.log('File size is bigger than 5Mb');
-    button.disabled=true;
     message.classList.remove('d-none');
   }
   else {
+    button.disabled=false;
     message.classList.add('d-none')
   }
 
